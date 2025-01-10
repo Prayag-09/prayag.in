46b8df2556d3527cafcfe4dd227d88d0a083ca81
Prayag T <prayag.t@zeronetraining.local>
2025-01-10 16:45:05 +0530

    footer completed

--- src/components/Sections/Footer.jsx
+++ src/components/Sections/Footer.jsx
@@ -1,8 +1,20 @@
 const Footer = () => {
-    return  (
-        <div>
-            Footer
-        </div>
-    )
-}
+	return (
+		<footer className='bg-primary py-6'>
+			<div className='max-w-4xl mx-auto px-6 md:px-8 text-center'>
+				<div className='flex flex-col items-center gap-2 md:flex-row md:justify-between'>
+					{/* <NowPlaying /> */}
+					<p>Spotify Music Here</p>
+					<p className='text-secondary font-medium'>
+						© 2025 Tushar Prayag | All Rights Reserved
+					</p>
+				</div>
+				<p className='text-secondary font-light text-sm mt-2'>
+					Made with ❤️ in India
+				</p>
+			</div>
+		</footer>
+	);
+};
+
 export default Footer;
