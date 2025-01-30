df323942c6b57a2605828ed9f651e39cda8ea0d5
T Prayag <prayagtushar2016@gmail.com>
2025-01-30 17:37:39 +0530

    Ready to Deploy

--- src/components/ui/3DCard.jsx
+++ src/components/ui/3DCard.jsx
@@ -16,12 +16,12 @@ export const CardContainer = ({ children, className, containerClassName }) => {
 		containerRef.current.style.transform = `rotateY(${x}deg) rotateX(${y}deg)`;
 	};
 
-	const handleMouseEnter = (e) => {
+	const handleMouseEnter = () => {
 		setIsMouseEntered(true);
 		if (!containerRef.current) return;
 	};
 
-	const handleMouseLeave = (e) => {
+	const handleMouseLeave = () => {
 		if (!containerRef.current) return;
 		setIsMouseEntered(false);
 		containerRef.current.style.transform = `rotateY(0deg) rotateX(0deg)`;
