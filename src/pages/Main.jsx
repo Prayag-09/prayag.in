672331b4125d508d043d180a06866319d41248d1
T Prayag <prayagtushar2016@gmail.com>
2025-01-30 17:50:35 +0530

    Ready to Deploy

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -4,18 +4,16 @@ import Projects from '../components/sections/Projects';
 import Contact from '../components/sections/Contact';
 import Blog from '../components/sections/Blog';
 import Footer from '../components/sections/Footer';
-import { Analytics } from '@vercel/analytics/react';
 const Main = () => {
 	return (
-		<main>
+		<div>
 			<Hero />
 			<About />
 			<Projects />
 			<Blog />
 			<Contact />
 			<Footer />
-			<Analytics />
-		</main>
+		</div>
 	);
 };
 
